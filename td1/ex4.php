25be3234c05550585c225e35e0b81a77c6aeede5
Alexandre Coué <62330232+AlexandreCoue@users.noreply.github.com>
2020-03-26 15:01:17 +0100

    edit ex 4

---
+++ td1/ex4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+		<meta charset="UTF-8">
+		<title>Exercice 4</title>
+	</head>
+<body>
+	<form method="POST">
+		<label for="size">Size : </label>
+    	<input type="number" value="" name="size" id="size">
+    	<input type="submit" value="Valider">
+	</form>
+	 <?php
+    	$ligne=$_GET["1"]??10;
+		$colon=$_GET["c"]??10;
+		
+		for ($i=0;$i<$ligne;$i++){
+			$bold=($i%2==0)?"bold":"normal";
+		for ($j%2==
+    ?>
+    <textarea name="texte"></textarea>
+</body>
+</html>
+
+# 
+# 
