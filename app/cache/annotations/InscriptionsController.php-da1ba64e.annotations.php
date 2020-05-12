2f3444b0691cf3a6290275aea4cff83f1a39aeb6
Alexandre Coué <62330232+AlexandreCoue@users.noreply.github.com>
2020-05-12 14:37:46 +0200

    Add files via upload

--- app/cache/annotations/InscriptionsController.php-da1ba64e.annotations.php
+++ app/cache/annotations/InscriptionsController.php-da1ba64e.annotations.php
@@ -9,5 +9,20 @@ return array(
   array (
   ),
 ),
+  'controllers\\InscriptionsController::index' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "/_default/","name"=>"index")
+  ),
+  'controllers\\InscriptionsController::inscription' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "inscription","methods"=>["get"],"name"=>"inscription")
+  ),
+  'controllers\\InscriptionsController::logout' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "/logout/","methods"=>["get"],"name"=>"logout")
+  ),
+  'controllers\\InscriptionsController::inscriptionList' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "inscriptions","methods"=>["get"],"name"=>"inscriptions")
+  ),
+  'controllers\\InscriptionsController::validateInscription' => array(
+    array('#name' => 'route', '#type' => 'Ubiquity\\annotations\\router\\RouteAnnotation', "do/inscription","methods"=>["get"],"name"=>"do.inscription")
+  ),
 );
 
--- app/controllers/InscriptionsController.php
+++ app/controllers/InscriptionsController.php
@@ -5,7 +5,44 @@ namespace controllers;
  **/
 class InscriptionsController extends ControllerBase{
 
+	/**
+	 *@route("/_default/","name"=>"index")
+	**/
 	public function index(){
 		
 	}
+
+
+	/**
+	 *@route("inscription","methods"=>["get"],"name"=>"inscription")
+	**/
+	public function inscription(){
+		
+	}
+
+
+	/**
+	 *@route("/logout/","methods"=>["get"],"name"=>"logout")
+	**/
+	public function logout(){
+		
+	}
+
+
+	/**
+	 *@route("inscriptions","methods"=>["get"],"name"=>"inscriptions")
+	**/
+	public function inscriptionList(){
+		
+	}
+
+
+	/**
+	 *@route("do/inscription","methods"=>["get"],"name"=>"do.inscription")
+	**/
+	public function validateInscription(){
+		
+	}
+	
+
 }
